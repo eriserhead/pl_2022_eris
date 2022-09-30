22ffa047e82a65ce5c9f423b709c41e50a84aca1
eriserhead <rendell.erise@neu.edu.ph>
2022-09-30 19:34:32 +0800

    Update Activity2.java

--- Activity2.java
+++ Activity2.java
@@ -1,57 +1,36 @@
 import java.util.*;
 
 public class Activity2 {
-
     public static void main(String[] args)
-
     {
-
         Scanner sc = new Scanner(System.in);
         System.out.print("INPUT STRING: ");
         String input = sc.nextLine();
-
         int state = 0;
-
         for (int i = 0; i < input.length(); i++)
-
         {
-
             if (state == 0 && input.charAt(i) == 'a')
-
                 state = 1;
-
             else if (state == 0 && input.charAt(i) == 'b')
-
                 state = 2;
-
             else if (state == 1 && input.charAt(i) == 'a')
-
                 state = 1;
-
             else if (state == 1 && input.charAt(i) == 'b')
+                state = 2;
+            else if (state == 2 && input.charAt(i) == 'a')
+                state = 1;
+            else if (state == 2 && input.charAt(i) == 'b')
 
                 state = 2;
-
-        
-
             else
-
             {
-
                 state = -1;
-
                 break;
-
             }
-
         }
-
         if (state == 1)
-
             System.out.println("String is Accepted");
-
         else
-
             System.out.println("String is Rejected");
 
     }
